c9995aec87d640ea6e8fc97b135782d81f3ac9bb
Diego Molina <skumblue@live.cl>
2013-07-15 16:27:28 -0400

    index 2

--- js/evento.js
+++ js/evento.js
@@ -32,7 +32,7 @@ $(document).ready(function(){
 //                  cuerpo+= '<div class="bloq3msjinner">';
 //                  cuerpo+=    '<div class="hacecuant">'+tiempo+'</div>';
 //                  cuerpo+= '</div></div></div>';
-             
+             //dassadds
              var pie = '<div class="bloq3msj"><div data-id="9" id="aceptarcoment" class="botonblue">Aceptar</div>';
                  pie+= '<div id="cancelar" class="botoncancel">Cancelar</div></div>';
                  
