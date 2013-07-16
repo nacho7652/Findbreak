438535ef15f460b2da83b0ad9d5b1b721e5881b1
Diego Molina <skumblue@live.cl>
2013-07-16 12:56:14 -0400

    eventoprofile prueba 3

--- sites/eventprofile.php
+++ sites/eventprofile.php
@@ -1,10 +1,10 @@
 <?php 
       echo 'hola'.$_GET['id'];
-      require_once '/findbreak/DAL/connect.php';
-      require_once '/findbreak/DAL/evento.php';
-      require_once '/findbreak/DAL/comentario.php';
-      require_once '/findbreak/DAL/usuario.php';
-      echo $_GET['id'];
+      require_once '../DAL/connect.php';
+      require_once '../DAL/evento.php';
+      require_once '../DAL/comentario.php';
+      require_once '../DAL/usuario.php';
+      echo 'chao'.$_GET['id'];
       $usuario = new usuario();
       $comentarioEvent = new comentario();
       $event = new evento();
