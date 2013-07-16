e1d0a888426fa93c15ccfe8279e7de3afa032ae4
Diego Molina <skumblue@live.cl>
2013-07-16 12:59:35 -0400

    arreglo de rutas parte 1

--- sites/eventprofile.php
+++ sites/eventprofile.php
@@ -1,10 +1,4 @@
 <?php 
-      echo 'hola'.$_GET['id'];
-//      require_once '../DAL/connect.php';
-//      require_once '../DAL/evento.php';
-//      require_once '../DAL/comentario.php';
-//      require_once '../DAL/usuario.php';
-      echo 'chao'.$_GET['id'];
       $usuario = new usuario();
       $comentarioEvent = new comentario();
       $event = new evento();
--- sites/userprofile.php
+++ sites/userprofile.php
@@ -1,9 +1,4 @@
 <?php 
-    
-      require_once '/DAL/connect.php';
-      require_once '/DAL/evento.php';
-      require_once '/DAL/comentario.php';
-      require_once '/DAL/usuario.php';
       $usuario = new usuario();
       $partid = explode('!', $_GET['id']);
       $usernameUrl = $partid[1];
